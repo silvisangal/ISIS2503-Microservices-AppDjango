c520589bd96c9ad25608774761acc310d4253cba
Juan Andrés Avelino Ospina <javelino2311@gmail.com>
2024-05-13 15:34:31 -0500

    Fix en deployment y variables de ambiente para db de variables

--- variables/monitoring/settings.py
+++ variables/monitoring/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'variables_db',
-        'USER': 'variables_user',
-        'PASSWORD': 'isis2503',
-        'HOST': '0.0.0.0',
+        'NAME': os.environ.get('VARIABLES_DB_NAME', 'variables_db')
+        'USER': os.environ.get('VARIABLES_DB_USER', 'variables_user')
+        'PASSWORD': os.environ.get('VARIABLES_DB_PASSWORD', 'isis2503')
+        'HOST': os.environ.get('VARIABLES_DB_HOST', 'localhost')
         'PORT': '5432',
     }
 }
