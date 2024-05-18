4eca9d996620a743ecfc3cd1cab359b146e61d45
Silvana Sandoval <s.sandovalg@uniandes.edu.co>
2024-05-18 10:10:27 -0500

    Cambios a places

--- places/places/urls.py
+++ places/places/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url, include
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^placelist/', views.PlaceList, name='placeList'),
+    url(r'^placecreate/$', csrf_exempt(views.PlaceCreate), name='placeCreate'),
 ]
