4b9147de8044be367777f11e56fe59b40f66c859
Silvana Sandoval <s.sandovalg@uniandes.edu.co>
2024-05-18 10:22:39 -0500

    Cambio a urls

--- places/monitoring/urls.py
+++ places/monitoring/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('variables.urls')),
+    path('', include('places.urls')),
 ]
 
 if settings.DEBUG is True:
